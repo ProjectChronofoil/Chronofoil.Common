a695b0f6f27c64396d68b88167265aec3dbde803
Liam <lmcintyre995@gmail.com>
2024-06-16 02:31:41 -0400

    Initial commit.

---
+++ src/Auth/Model.cs
@@ -0,0 +1,28 @@
+﻿// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnassignedField.Global
+// ReSharper disable CollectionNeverUpdated.Global
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace Chronofoil.Common.Auth;
+
+public class AuthRequest
+{
+    public string AuthorizationCode { get; set; }
+}
+
+public class RefreshRequest
+{
+    public string RefreshToken { get; set; }
+}
+
+public class AccessTokenResponse
+{
+    public string TokenType { get; } = "Bearer";
+    public required string AccessToken { get; init; }
+    public required string RefreshToken { get; init; }
+    public required long ExpiresIn { get; init; } // seconds
+}
+
+public class AcceptTosRequest
+{
+    public int TosVersion { get; init; }
+}
